6a1f7174b1bdd093791cbb51f3377d949da4c9d3
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-27 18:36:55 +0530

    changed on 27th April 2022

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
-const HeroBanner = () => {
+
+import { urlFor } from '../lib/client';
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className='hero-banner-container'>
       <div>
         <p className='beats-solo'>
-          SMALL TEXT
+          {heroBanner.smallText}
         </p>
         <h3>
-          MID TEXT
+        {heroBanner.midText}
         </h3>
-        <img src="" alt="headphones" className='hero-banner-image'/>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image'/>
         <div>
-          <Link href="/ID">
-              <button type='button'>BUTTON TEXT</button>
+          <Link href={`/${heroBanner.product}`}>
+              <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
