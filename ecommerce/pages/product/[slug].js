ecda5f43477e20447d3d910a170d6bb6a8e45ad9
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-28 12:27:21 +0530

    changed on 28th April 2022

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = () => {
     return (
-        <div>Product</div>
+        <div>
+            <div className='product-detail-container'>
+                <div>
+                    <div className='image-container'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-
+export const getStaticProps = async () => {
+    const query = '*[_type == "product"]'
+    const products = await client.fetch(query);
+    const bannerQuery = '*[_type == "banner"]'
+    const bannerData = await client.fetch(bannerQuery);
+  
+    return {
+      props: { products, bannerData }
+    }
+  }
 export default ProductDetails
