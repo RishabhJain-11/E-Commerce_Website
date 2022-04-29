962bfe7e85d927e1f8e7c700a5e040c7827715e4
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-29 21:36:43 +0530

    changed on 29th April 2022

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -33,6 +33,15 @@ export const StateContext = ({ children }) => {
         }
         toast.success(`${qty} ${product.name} added to cart.`);
     }
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id);
+        const newCartItems = cartItems.filter((item) => item._id !== product._id);
+    
+        setTotalPrice((prevTotalPrice) => prevTotalPrice -foundProduct.price * foundProduct.quantity);
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+        setCartItems(newCartItems);
+      }
+    
 
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id)
