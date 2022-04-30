63f138922675f20db604fc208c062a682995588f
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-30 13:38:46 +0530

    changed on 30th April 2022

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -15,7 +15,7 @@ const HeroBanner = ({heroBanner}) => {
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image'/>
         <div>
-          <Link href={`/${heroBanner.product}`}>
+          <Link href={`/product/${heroBanner.product}`}>
               <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
